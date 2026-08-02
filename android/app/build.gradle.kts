7924ad8dc112a77fec3e76f5e551b1b0551d9b78
velezalarconjuanpablo-alt <velezalarconjuanpablo@gmail.com>
2026-08-02 01:55:11 +0000

    fix: subir compileSdk a 36 para file_picker

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -6,7 +6,7 @@ plugins {
 
 android {
     namespace = "com.example.notes_app"
-    compileSdk = flutter.compileSdkVersion
+    compileSdk = 36
     ndkVersion = flutter.ndkVersion
 
     compileOptions {
